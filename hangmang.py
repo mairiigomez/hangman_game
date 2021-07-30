dfcc5fd0094c17a37af90de5b4359e5269cba004
mairiigomez <80441805+mairiigomez@users.noreply.github.com>
2021-07-29 19:17:26 -0500

    Update hangmang.py
    Changed position of run function

--- hangmang.py
+++ hangmang.py
@@ -1,4 +1,4 @@
-"""Hagman game version 2 
+"""Hangman game version 2 
 Date: July 26, 2021 
 Created by: Mairiyisel """
 
@@ -12,6 +12,65 @@ def read_data():
     with open("./data.txt","r",encoding="utf-8") as file:
         list_of_words = file.readlines()
         return list_of_words
+def run():
+    clean_screen()
+    print(
+        '''
+   **  **    ***    **   **  *******  **      **    ***    **   **
+   **  **   ** **   ***  **  **       ***    ***   ** **   ***  **
+   ******   *****   **** **  ** ****  ****  ****   *****   **** **
+   **  **  **   **  ** ****  **   **  ** **** **  **   **  ** ****
+   **  **  **   **  **  ***  *******  **  **  **  **   **  **  ***
+    ''')
+    os.system('pause')
+
+    list_of_words = read_data()
+    word = random.choice(list_of_words)
+    chosen_word = list(word.strip().upper())
+    #lenght_chosen_word = len(chosen_word)
+    list_letters = ['-']*len(chosen_word)
+    #print(list_letters)
+    number_of_tries = 0
+    failure = 0
+    clean_screen()
+    draw_hangman(failure, list_letters)
+    while list_letters != chosen_word and failure < 6:
+        position = 0
+        user_fail = True
+        letter = input("Choose a letter: ")
+        user_letter = letter.upper()
+        try: 
+            if user_letter.isnumeric() == True:
+                raise ValueError("Please, Enter one LETTER")
+            elif len(user_letter) > 1:
+                raise ValueError("Please, enter just one letter")
+            #elif number_of_tries > 7:
+             #   raise ValueError("Game Over!")
+            for guess_letter in chosen_word:
+                if guess_letter == user_letter:
+                    list_letters[position] = user_letter
+                    user_fail = False
+                position += 1
+            number_of_tries += 1
+            
+            if user_fail == True:
+                failure += 1
+
+            clean_screen()
+            draw_hangman(failure, list_letters)
+        
+        except ValueError as ve:
+            print(ve)     
+            
+    clean_screen()
+    
+    if list_letters == chosen_word:
+        user_won = f'¡You won! it took you {number_of_tries} times to figure it out'
+        print(user_won) 
+    else:
+        draw_hangman(failure, list_letters)
+        print("GAME OVER")   
+ 
 
 def draw_hangman(failure, list_of_letters):
     hangman_list = [
@@ -167,67 +226,6 @@ def draw_hangman(failure, list_of_letters):
         print(hangman_list[len(hangman_list)-1], list_of_letters)
     else:
         print(hangman_list[failure],list_of_letters)
-
-def run():
-    clean_screen()
-    print(
-        '''
-   **  **    ***    **   **  *******  **      **    ***    **   **
-   **  **   ** **   ***  **  **       ***    ***   ** **   ***  **
-   ******   *****   **** **  ** ****  ****  ****   *****   **** **
-   **  **  **   **  ** ****  **   **  ** **** **  **   **  ** ****
-   **  **  **   **  **  ***  *******  **  **  **  **   **  **  ***
-    ''')
-    os.system('pause')
-
-    list_of_words = read_data()
-    word = random.choice(list_of_words)
-    chosen_word = list(word.strip().upper())
-    #lenght_chosen_word = len(chosen_word)
-    list_letters = ['-']*len(chosen_word)
-    #print(list_letters)
-    number_of_tries = 0
-    failure = 0
-    clean_screen()
-    draw_hangman(failure, list_letters)
-    while list_letters != chosen_word and failure < 6:
-        position = 0
-        user_fail = True
-        letter = input("Choose a letter: ")
-        user_letter = letter.upper()
-        try: 
-            if user_letter.isnumeric() == True:
-                raise ValueError("Please, Enter one LETTER")
-            elif len(user_letter) > 1:
-                raise ValueError("Please, enter just one letter")
-            #elif number_of_tries > 7:
-             #   raise ValueError("Game Over!")
-            for guess_letter in chosen_word:
-                if guess_letter == user_letter:
-                    list_letters[position] = user_letter
-                    user_fail = False
-                position += 1
-            number_of_tries += 1
-            
-            if user_fail == True:
-                failure += 1
-
-            clean_screen()
-            draw_hangman(failure, list_letters)
-        
-        except ValueError as ve:
-            print(ve)
-        
-
-
-    clean_screen()
-    
-    if list_letters == chosen_word:
-        user_won = f'¡You won! it took you {number_of_tries} times to figure it out'
-        print(user_won) 
-    else:
-        draw_hangman(failure, list_letters)
-        print("GAME OVER")   
 
 if __name__ == '__main__':
     run()
